af01f7e63d76b2e363fab1c2e61afe901d75f488
Aditya-NV-06 <adityanv4@gmail.com>
2025-01-02 23:37:26 +0530

    Add react-icons dependency and display GitHub stars in NavBar

--- src/app/nav.tsx
+++ src/app/nav.tsx
@@ -1,13 +1,34 @@
-import GitHubButton from "../components/GitHubButton";
+"use client";
+import { FaGithub } from 'react-icons/fa';
+import { useState,useEffect } from 'react';
 
 const NavBar = () => {
+  //to state for the number of stars of the repository
+   const [stars,setStars] = useState<number |null> (null)
+  //fetching the number of stars of the repository
+   useEffect(() =>{ 
+    fetch("https://api.github.com/repos/FOSSUChennai/Communities")
+    .then((res) => res.json())
+    .then((data) => {
+      setStars(data.stargazers_count)
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+
+
+   })
     return (
       <nav
         className="w-full flex-1 bg-[#fafafa] text-black py-4 px-4 md:px-8 lg:px-16 flex justify-between items-center"
       >
         <a href="#" className="text-xl font-semibold">tamilnadu.tech</a>
         <div className="space-x-4">
-        <GitHubButton />
+
+          <a className="px-4 py-2 text-[#667085]  border shadow-sm rounded-md" href="https://github.com/FOSSUChennai/Communities">
+            <FaGithub className="inline-block m-2 mb-3 " />
+            {stars !== null ? <span className='text-base'>{stars} ★</span> : <span className='text-base'>Loading...</span>} 
+          </a>
         </div>
       </nav>
     );

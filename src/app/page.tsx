b3c1b9fd2deda3385f4e4a7f7943a2e7b227cdaf
surendar-05 <surendarrockzz7@gmail.com>
2025-01-04 16:34:47 +0530

    Fixed Header Alignment and Updated the footer Text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         async
         defer
       />
-      <div className="bg-[#fafafa]">
+      <div className="bg-[#fafafa] min-h-screen flex flex-col">
         <div className="max-w-[1120px] mx-auto font-inter">
           <NavBar />
           <Hero />

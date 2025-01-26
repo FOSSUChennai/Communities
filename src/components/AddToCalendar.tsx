543f53457a2cd99339d40ee52025bfa15a654093
Mr. Prashanth Chowdary <ch.prashanthchowdary007@gmail.com>
2025-01-26 23:54:49 +0530

    feat(calendar-preff): unstable 1.0

--- src/components/AddToCalendar.tsx
+++ src/components/AddToCalendar.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
 import Add2Calendar from '../../public/add2Calendar.webp';
+import Popup from './Popup';
+import { useState } from 'react';
 
 interface AddToCalendarProps {
   eventTitle: string;
@@ -14,6 +16,12 @@ const AddToCalendar: React.FC<AddToCalendarProps> = ({
   eventDate,
   eventLink
 }) => {
+  const [isPopup, setIsPopup] = useState(false);
+  const [PopupData, setPopupData] = useState({
+    isChecked: false,
+    option: ''
+  });
+  console.log(PopupData);
   const dateFormatter = (formatDate: string): string => {
     const date = new Date(formatDate);
     const year = date.getFullYear().toString();
@@ -22,24 +30,56 @@ const AddToCalendar: React.FC<AddToCalendarProps> = ({
     return `${year}${month}${day}`;
   };
 
+  const handlePopupData = (data: { isChecked: boolean; option: string }) => {
+    setPopupData(data);
+  };
+
   const handleRedirect = (event: React.MouseEvent<HTMLButtonElement>): void => {
     event.preventDefault();
-    const params = new URLSearchParams({
-      action: 'TEMPLATE',
-      text: eventTitle,
-      details: `Read on this page ${eventLink}`,
-      location: eventVenue,
-      dates: `${dateFormatter(eventDate)}/${dateFormatter(eventDate)}`
-    });
-    const baseUrl = 'https://calendar.google.com/calendar/render';
-    const url = `${baseUrl}?${params.toString()}`;
-    window.open(url, '_blank');
+    const value = localStorage.getItem('preff-Calendar');
+    console.log(value, PopupData.isChecked, PopupData.option);
+    if (!value && !PopupData.isChecked && !PopupData.option) {
+      setIsPopup(true);
+      return;
+    }
+    if (PopupData.option.toString() === 'google' || value === 'google') {
+      event.preventDefault();
+      const params = new URLSearchParams({
+        action: 'TEMPLATE',
+        text: eventTitle,
+        details: `Read on this page ${eventLink}`,
+        location: eventVenue,
+        dates: `${dateFormatter(eventDate)}/${dateFormatter(eventDate)}`
+      });
+      const baseUrl = 'https://calendar.google.com/calendar/render';
+      const url = `${baseUrl}?${params.toString()}`;
+      window.open(url, '_blank');
+    } else if (PopupData.option.toString() === 'outlook' || value === 'outlook') {
+      event.preventDefault();
+      const params = new URLSearchParams({
+        path: '/calendar/action/compose',
+        subject: eventTitle,
+        body: `Read on this page ${eventLink}`,
+        location: eventVenue,
+        startdt: dateFormatter(eventDate),
+        enddt: dateFormatter(eventDate)
+      });
+      const baseUrl = 'https://outlook.office.com';
+      const url = `${baseUrl}?${params.toString()}`;
+      window.open(url, '_blank');
+    }
   };
 
   return (
-    <button onClick={handleRedirect}>
-      <Image src={Add2Calendar} alt='CalendarIcon' style={{ width: '20px', height: '20px' }} />
-    </button>
+    <>
+      {isPopup ? (
+        <Popup sendData={handlePopupData} />
+      ) : (
+        <button onClick={handleRedirect}>
+          <Image src={Add2Calendar} alt='CalendarIcon' style={{ width: '20px', height: '20px' }} />
+        </button>
+      )}
+    </>
   );
 };
 

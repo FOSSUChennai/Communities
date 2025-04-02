762ff6ebdc0fc96205c739aabbdd15b6865b931e
Mohammed Thaha <mohammedthahacse@gmail.com>
2025-04-02 21:52:49 +0530

    fix: rocket image in calltoaction

--- src/components/pages/home/calltoaction.tsx
+++ src/components/pages/home/calltoaction.tsx
@@ -6,36 +6,8 @@ import { useRouter } from 'next/navigation';
 const CallToAction = () => {
   const router = useRouter();
   return (
-
-    <div className='p-4 md:p-8 lg:p-16'>
-      <div className='relative rounded-xl bg-[#4CAF50] dark:bg-[#2a9531]'>
-        <div className='flex items-center justify-between p-8'>
-          <div className='max-w-2xl'>
-            <h3 className='mb-6 text-center text-2xl font-normal text-white dark:text-gray-200 md:text-left md:text-3xl'>
-              Know a tech event? <br /> Share it to help others find and join by adding yours to the
-              list!
-            </h3>
-            <div className='flex flex-wrap justify-center gap-4 md:justify-start'>
-              <button
-                className='rounded-md bg-black px-6 py-2 text-white transition-colors hover:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-300'
-                onClick={() =>
-                  window.open(
-                    'https://github.com/FOSSUChennai/Communities/blob/main/CONTRIBUTING.md'
-                  )
-                }
-              >
-                Contribute
-              </button>
-              <button
-                className='rounded-md border-2 border-white bg-transparent px-6 py-2 text-white transition-colors hover:bg-white/10 dark:border-2 dark:border-gray-700'
-                onClick={() => window.open('https://fossunited.org/c/chennai')}
-              >
-                Visit Us
-              </button>
-            </div>
-
     <div className='m-[12px] px-6 py-12 md:px-16'>
-      <div className='relative rounded-lg bg-[#4CAF50] p-10 shadow-lg'>
+      <div className='relative rounded-lg bg-[#4CAF50] p-10 shadow-lg dark:bg-green-600'>
         <div className='absolute bottom-0 right-0 hidden translate-x-[72px] md:block'>
           <Image
             src='/rockethalf.png'
@@ -73,7 +45,6 @@ const CallToAction = () => {
             >
               Communities
             </button>
-
           </div>
         </div>
       </div>
